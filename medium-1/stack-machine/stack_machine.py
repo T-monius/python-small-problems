977ebc0b1cf8dff6f615e7ebe397197eb035fda9
tmo macbook <trrl.mo@hotmail.com>
2022-03-16 09:22:43 -0400

    Refactor stack_machine module

--- medium-1/stack-machine/stack_machine.py
+++ medium-1/stack-machine/stack_machine.py
@@ -11,42 +11,76 @@ def print_to_mini_output(mini_output, val):
 
     return mini_output
 
+
 def is_negative_numeric(num):
     '''Determine if a string contains only a negative number'''
     return num[0] == '-' and num[1:].isnumeric()
 
 
+def add(val, other_val):
+    '''Add a value to another'''
+    return other_val + val
+
+
+def subtract(val, other_val):
+    '''Subtract a value from another'''
+    return other_val - val
+
+
+def multiply(val, other_val):
+    '''Multiply a value by another'''
+    return other_val * val
+
+
+def divide(val, other_val):
+    '''Return division of a value from another if val is not zero'''
+    if other_val > 0:
+        return int(other_val / val)
+
+    return None
+
+
+def modulus(val, other_val):
+    '''Return the modulus of a value from another if val is not zero'''
+    if other_val > 0:
+        return other_val % val
+
+    return None
+
+
+def compute(val, other_val, command):
+    '''Calculate a result operating on two values with a given command'''
+    computations = {
+        'ADD': add,
+        'SUB': subtract,
+        'MULT': multiply,
+        'DIV': divide,
+        'MOD': modulus,
+    }
+    result = computations[command](val, other_val)
+
+    return result
+
+
 def minilang(commands):
     '''A function that operates with a stack and register to implement
        a basic programming language'''
-    cmds = commands.split()
     register = 0
     stack = []
     mini_output = ''
 
-    for command in cmds:
-        if command == 'PRINT':
-            mini_output = print_to_mini_output(mini_output, register)
-        if command == 'ADD':
-            val = stack.pop()
-            register += val
-        if command == 'SUB':
-            val = stack.pop()
-            register -= val
-        if command == 'MULT':
-            val = stack.pop()
-            register *= val
-        if command == 'DIV' and register > 0:
-            val = stack.pop()
-            register = int(register / val)
-        if command == 'MOD':
-            val = stack.pop()
-            register %= val
-        if command == 'POP':
-            register = stack.pop()
-        if command == 'PUSH':
-            stack.append(register)
-        if command.isnumeric() or is_negative_numeric(command):
+    for command in commands.split():
+        if command[0] == 'P':
+            if command == 'POP':
+                register = stack.pop()
+            elif command == 'PUSH':
+                stack.append(register)
+            elif command == 'PRINT':
+                mini_output = print_to_mini_output(mini_output, register)
+        elif command.isnumeric() or is_negative_numeric(command):
             register = int(command)
+        else:
+            val = stack.pop()
+            register = compute(val, register, command)
 
     return mini_output
