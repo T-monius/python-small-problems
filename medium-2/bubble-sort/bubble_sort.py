2eb533114479b340e062dedf731dc59a77dcf0d8
tmo macbook <trrl.mo@hotmail.com>
2022-04-13 09:09:32 -0400

    Refactor bubble_sort.

--- medium-2/bubble-sort/bubble_sort.py
+++ medium-2/bubble-sort/bubble_sort.py
@@ -1,19 +1,29 @@
 '''A module to implement the bubble sort algorithm'''
 
-def sort_the_bubble_way(values):
+def pass_over_values_swapping_in_place(values, swaps):
+    '''Make a pass over values swapping consecutive values where the
+       predecessor is greater than the successor counting swaps as
+       they occur. Return the count of swaps that occurred during the
+       pass.'''
+    limit = len(values) - 1
+    for idx in range(0, limit):
+        val = values[idx]
+        next_val = values[idx + 1]
+        if val > next_val:
+            swaps += 1
+            values[idx], values[idx + 1] = values[idx + 1], values[idx]
+
+    return swaps
+
+
+def sort_the_bubble_way_in_place(values):
     '''Sort the input list in place and return'''
     passes = 0
     swaps = None
 
     while passes < 1 or swaps > 0:
         swaps = 0
-        next_val = None
-        for idx, val in enumerate(values):
-            if idx < len(values) - 1:
-                next_val = values[idx + 1]
-            if next_val and (val > next_val):
-                swaps += 1
-                values[idx], values[idx + 1] = values[idx + 1], values[idx]
+        swaps = pass_over_values_swapping_in_place(values, swaps)
 
         passes += 1
 
--- medium-2/bubble-sort/test_bubble_sort.py
+++ medium-2/bubble-sort/test_bubble_sort.py
@@ -1,28 +1,27 @@
 '''A module for testing the bubble_sort module'''
 import unittest
-from bubble_sort import sort_the_bubble_way
+from bubble_sort import sort_the_bubble_way_in_place
 
 class TestBubbleSort(unittest.TestCase):
     '''A class for testing the bubble_sort module'''
-    @unittest.skip('Buggy.')
     def test_a_two_element_list(self):
         '''Test a list of just two integers'''
         two_nums = [5, 3]
-        sorted_nums = sort_the_bubble_way(two_nums)
+        sorted_nums = sort_the_bubble_way_in_place(two_nums)
         self.assertEqual(sorted_nums, [3, 5])
 
 
     def test_a_multiple_element_list(self):
         '''Test a list of multiple elements'''
         nums = [2, 6, 1, 4, 7]
-        sorted_nums = sort_the_bubble_way(nums)
+        sorted_nums = sort_the_bubble_way_in_place(nums)
         self.assertEqual(sorted_nums, [1, 2, 4, 6, 7])
 
-    @unittest.skip('Not quite working.')
+
     def test_a_list_of_names(self):
         '''Test a list of names'''
         names = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie']
-        sorted_nums = sort_the_bubble_way(names)
+        sorted_nums = sort_the_bubble_way_in_place(names)
         self.assertEqual(sorted_nums, ['Alice', 'Bonnie', 'Kim', 'Pete', 'Rachel', 'Sue', 'Tyler'])
 
 
