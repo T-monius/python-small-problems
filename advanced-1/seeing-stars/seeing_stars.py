49a59334250965a3c23192103f9bae8c947b7b57
tmo macbook <trrl.mo@hotmail.com>
2022-04-20 09:05:22 -0400

    Refactor module for creating star grids

--- advanced-1/seeing-stars/seeing_stars.py
+++ advanced-1/seeing-stars/seeing_stars.py
@@ -1,24 +1,53 @@
 '''A module for outputing star grids by certain dimensions'''
 
+SPACE = ' '
+STAR = '*'
+
+def calculate_spaces(line, middle, last_idx):
+    '''Determine how many leading spaces and spaces between stars
+       based on line, middle line, and last index'''
+    is_before_middle = line < middle
+
+    if is_before_middle:
+        distance_from_middle = middle - line
+        leading_space = line
+    else:
+        distance_from_middle = line - middle
+        leading_space = last_idx - line
+    spaces = distance_from_middle - 1
+
+    return leading_space, spaces
+
+
+def middle_line(stars):
+    '''Return a line of n stars based on the input'''
+    middle_gridline = f"{STAR * stars}\n"
+
+    return middle_gridline
+
+
+def starry_line(line, middle, last_idx):
+    '''Return a starry line based on the current line, middle line,
+       and last index'''
+    leading_space, spaces = calculate_spaces(line, middle, last_idx)
+
+    gridline = f'{SPACE * leading_space}{STAR}' \
+               f'{SPACE * spaces}{STAR}' \
+               f'{SPACE * spaces}{STAR}\n'
+
+    return gridline
+
+
 def star_power_grid(rows):
     '''Return a sring of line separated rows of asterisks spaced
-       per row number to form an 8 point star'''
+       per line number to form an 8 point star'''
     middle = int(rows / 2)
     last_idx = rows - 1
     grid = ''
-    for row in range(0, rows):
-        if row == middle:
-            grid += f"{'*' * rows}\n"
-            continue
-        is_before_middle = row < middle
-        if is_before_middle:
-            distance_from_middle = middle - row
-            leading_space = row
-        else:
-            distance_from_middle = row - middle
-            leading_space = last_idx - row
-        spaces = distance_from_middle - 1
-        gridline = f'{" " * leading_space}*{" " * spaces}*{" " * spaces}*\n'
+
+    for line in range(0, rows):
+        gridline = middle_line(rows) if line == middle else starry_line(line, middle, last_idx)
+
         grid += gridline
 
     return grid
--- advanced-1/seeing-stars/test_seeing_stars.py
+++ advanced-1/seeing-stars/test_seeing_stars.py
@@ -2,6 +2,15 @@
 import unittest
 from seeing_stars import star_power_grid
 
+SEVEN_ROW_GRID = '*  *  *\n' \
+                 ' * * *\n' \
+                 '  ***\n' \
+                 '*******\n' \
+                 '  ***\n' \
+                 ' * * *\n' \
+                 '*  *  *\n'
+
+
 NINE_ROW_GRID = '*   *   *\n' \
                  ' *  *  *\n' \
                  '  * * *\n' \
@@ -14,6 +23,12 @@ NINE_ROW_GRID = '*   *   *\n' \
 
 class TestSeeingStars(unittest.TestCase):
     '''A class for testing the seeing_stars module'''
+    def test_seven_rows(self):
+        '''Test creating a 7x7 grid'''
+        rows = 7
+        grid = star_power_grid(rows)
+        self.assertEqual(grid, SEVEN_ROW_GRID)
+
     def test_nine_rows(self):
         '''Test creating a 9x9 grid'''
         rows = 9
